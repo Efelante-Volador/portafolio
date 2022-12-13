ecc0d7eaa629a40e9b778e6008f71b0fd519eafa
Replit user <>
2022-12-13 13:51:38 +0000

    mejora de cambios de idioma

--- js/idioma.js
+++ js/idioma.js
@@ -1,26 +1,34 @@
 document.getElementById('jsonBtnEs').addEventListener('click', es);
 document.getElementById('jsonBtnEn').addEventListener('click', en);
+
+// Obtiene un elemento del documento HTML Párrafo
+var titulo1 = document.getElementById('t1');
+var titulo2 = document.getElementById('t2');
+var navTittle1 = document.getElementById('nav-tittle-inicio');
+var navTittle2 = document.getElementById('nav-tittle-perfil');
+var navTittle3 = document.getElementById('nav-tittle-portafolio');
+var navTittle4 = document.getElementById('nav-tittle-contacto');
+var navTittle5 = document.getElementById('nav-tittle-idioma');
+var caption1 = document.getElementById('caption1');
+var caption2 = document.getElementById('caption2');
+var caption3 = document.getElementById('caption3');
+var caption4 = document.getElementById('caption4');
+var tituloPerfil = document.getElementById('perfil-titulo');
+var txt1 = document.getElementById('cap1');
+var txt2 = document.getElementById('cap2');
+var txt3 = document.getElementById('cap3');
+var txt4 = document.getElementById('cap4');
+
 function es() {
-  //console.log("spanish");
+  var idioma = document.getElementById('jsonBtnEs');
+  console.log(idioma.dataset.value);
+  //console.log("Spanish");
   fetch('json/infopag.json')
     .then(function(res) {
       // Devuelve el contenido del archivo JSON parseado como un objeto JavaScript
       return res.json();
     })
     .then(function(json) {
-      // Obtiene un elemento del documento HTML Párrafo
-      var titulo1 = document.getElementById('t1');
-      var titulo2 = document.getElementById('t2');
-      var navTittle1 = document.getElementById('nav-tittle-inicio');
-      var navTittle2 = document.getElementById('nav-tittle-perfil');
-      var navTittle3 = document.getElementById('nav-tittle-portafolio');
-      var navTittle4 = document.getElementById('nav-tittle-contacto');
-      var navTittle5 = document.getElementById('nav-tittle-idioma');
-      var caption1 = document.getElementById('caption1');
-      var caption2 = document.getElementById('caption2');
-      var caption3 = document.getElementById('caption3');
-      var caption4 = document.getElementById('caption4');
-      var tituloPerfil = document.getElementById('perfil-titulo');
       // Establece el contenido del archivo JSON como el texto del párrafo
       titulo1.textContent = json.es.titles.t1;
       titulo2.textContent = json.es.titles.t2;
@@ -33,7 +41,11 @@ function es() {
       caption2.textContent = json.es.profile.caption.c2;
       caption3.textContent = json.es.profile.caption.c3;
       caption4.textContent = json.es.profile.caption.c4;
-      tituloPerfil.textcontent = json.es.profile.title;
+      tituloPerfil.textContent = json.es.profile.title;
+      txt1.textContent = json.es.profile.text.txt1;
+      txt2.textContent = json.es.profile.text.txt2;
+      txt3.textContent = json.es.profile.text.txt3;
+      txt4.textContent = json.es.profile.text.txt4;
     });
 }
 function en() {
@@ -44,19 +56,6 @@ function en() {
       return res.json();
     })
     .then(function(json) {
-      // Obtiene un elemento del documento HTML Párrafo
-      var titulo1 = document.getElementById('t1');
-      var titulo2 = document.getElementById('t2');
-      var navTittle1 = document.getElementById('nav-tittle-inicio');
-      var navTittle2 = document.getElementById('nav-tittle-perfil');
-      var navTittle3 = document.getElementById('nav-tittle-portafolio');
-      var navTittle4 = document.getElementById('nav-tittle-contacto');
-      var navTittle5 = document.getElementById('nav-tittle-idioma');
-      var caption1 = document.getElementById('caption1');
-      var caption2 = document.getElementById('caption2');
-      var caption3 = document.getElementById('caption3');
-      var caption4 = document.getElementById('caption4');
-      var tituloPerfil = document.getElementById('perfil-titulo');
       // Establece el contenido del archivo JSON como el texto del párrafo
       titulo1.textContent = json.en.titles.t1;
       titulo2.textContent = json.en.titles.t2;
@@ -70,5 +69,11 @@ function en() {
       caption3.textContent = json.en.profile.caption.c3;
       caption4.textContent = json.en.profile.caption.c4;
       tituloPerfil.textContent = json.en.profile.title;
+      txt1.textContent = json.en.profile.text.txt1;
+      txt2.textContent = json.en.profile.text.txt2;
+      txt3.textContent = json.en.profile.text.txt3;
+      txt4.textContent = json.en.profile.text.txt4;
     });
 }
+
+
