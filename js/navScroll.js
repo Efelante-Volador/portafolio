2f8551825ffda0176265f01e72a9f30a11c8926b
Replit user <>
2022-12-19 17:35:56 +0000

    se agrego mejora movil de scroll, se agrego animacion a imagenes de proyectos

--- js/navScroll.js
+++ js/navScroll.js
@@ -9,15 +9,28 @@ window.addEventListener('scroll', function() {
   //Este codigo obtiene la ubicacion del Scroll
   let ubicacionactual = window.pageYOffset || document.documentElement.scrollTop
   console.log(ubicacionactual);
-
+  if (/Mobi/.test(navigator.userAgent)) {
+    // El usuario está accediendo a tu sitio desde un dispositivo móvil
+    if (ubicacionactual >= 600) {
+      $nav.style.visibility = "visible";
+    }
+    else {
+      $nav.style.visibility = "hidden";
+    }
+  } else {
+    // El usuario está accediendo a tu sitio desde una computadora de escritorio
+    if (ubicacionactual >= 630) {
+      $nav.style.visibility = "visible";
+    }
+    else {
+      $nav.style.visibility = "hidden";
+    }
+  }
   //Este codigo hace aparecer o desaparecer el NavBar dependiendo de donde se encuentre el Scroll
-  if (ubicacionactual >= 630) {
-    $nav.style.visibility = "visible";
-  }
-  else {
-    $nav.style.visibility = "hidden";
-  }
+
 })
+
+
 //Esta funcion desplaza el navegador a la seccion de inicio
 function inicio() {
   window.scrollTo("smooth", 0);
@@ -26,13 +39,27 @@ function inicio() {
 }
 //Esta funcion desplaza el navegador a la seccion de Perfil
 function miTrabajo() {
-  window.scrollTo("smooth", 782);
-  sobremi.style.color = "rgb(38, 222, 170)";
-  porta.style.color = "rgba(255, 255, 255, 0.55)"
+  if (/Mobi/.test(navigator.userAgent)) {
+    window.scrollTo("smooth", 692);
+    sobremi.style.color = "rgb(38, 222, 170)";
+    porta.style.color = "rgba(255, 255, 255, 0.55)"
+  } else {
+    window.scrollTo("smooth", 782);
+    sobremi.style.color = "rgb(38, 222, 170)";
+    porta.style.color = "rgba(255, 255, 255, 0.55)"
+  }
 }
 //Esta funcion desplaza el navegador a la seccion de Proyectos
 function portafolio() {
-  window.scrollTo("smooth", 1717);
-  porta.style.color = "rgb(38, 222, 170)";
-  sobremi.style.color = "rgba(255, 255, 255, 0.55)"
+
+  if (/Mobi/.test(navigator.userAgent)) {
+    window.scrollTo("smooth", 2594);
+    porta.style.color = "rgb(38, 222, 170)";
+    sobremi.style.color = "rgba(255, 255, 255, 0.55)"
+  } else {
+    window.scrollTo("smooth", 1769);
+    porta.style.color = "rgb(38, 222, 170)";
+    sobremi.style.color = "rgba(255, 255, 255, 0.55)"
+  }
 }
+                //4494 Movil //2600 Escritorio  //Certificados
