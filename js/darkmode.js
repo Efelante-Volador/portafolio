8f15b57849d9f1ebb87dbfc169a09c845f53ca84
FabianLopezMagnan <fabianlopezmagnan@gmail.com>
2023-01-16 20:43:12 +0000

    Subida de datos

--- js/darkmode.js
+++ js/darkmode.js
@@ -5,13 +5,13 @@ const toggleText = document.getElementById("texto-toggle");
 window.onload = function() {
   var toggle = localStorage.getItem("toggle");
   if (toggle == "dark") {
-    toggleIcon.src = '../img/icons/sun.png'
+    toggleIcon.src = './img/icons/sun.png'
     toggleText.textContent = 'Light Mode';
     localStorage.setItem("toggle", "dark");
     document.body.classList.add("dark");
   }
   else {
-    toggleIcon.src = '../img/icons/moon.png'
+    toggleIcon.src = './img/icons/moon.png'
     toggleText.textContent = 'Dark Mode';
     localStorage.setItem("toggle", "light");
     document.body.classList.remove("dark");
