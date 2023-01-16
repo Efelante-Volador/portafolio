1b959cdb7b1c5622d48f034276256cf5391ae747
FabianLopezMagnan <fabianlopezmagnan@gmail.com>
2023-01-16 21:25:01 +0000

    Actualizacion variables almacenadas

--- js/funcionesProyectos.js
+++ js/funcionesProyectos.js
@@ -53,6 +53,21 @@ window.onload = function() {
   else {
     idiomaEs();
   }
+  const toggleIcon = document.getElementById("toggle-icon");
+  const toggleText = document.getElementById("texto-toggle");
+  var toggle = localStorage.getItem("toggle");
+  if (toggle == "dark") {
+    toggleIcon.src = './img/icons/sun.png'
+    toggleText.textContent = 'Light Mode';
+    localStorage.setItem("toggle", "dark");
+    document.body.classList.add("dark");
+  }
+  else {
+    toggleIcon.src = './img/icons/moon.png'
+    toggleText.textContent = 'Dark Mode';
+    localStorage.setItem("toggle", "light");
+    document.body.classList.remove("dark");
+  }
 }
 function idiomaEs() {
   fetch('../json/project.json')
--- js/idioma.js
+++ js/idioma.js
@@ -52,6 +52,21 @@ window.onload = function() {
   else {
     es();
   }
+  const toggleIcon = document.getElementById("toggle-icon");
+  const toggleText = document.getElementById("texto-toggle");
+  var toggle = localStorage.getItem("toggle");
+  if (toggle == "dark") {
+    toggleIcon.src = './img/icons/sun.png'
+    toggleText.textContent = 'Light Mode';
+    localStorage.setItem("toggle", "dark");
+    document.body.classList.add("dark");
+  }
+  else {
+    toggleIcon.src = './img/icons/moon.png'
+    toggleText.textContent = 'Dark Mode';
+    localStorage.setItem("toggle", "light");
+    document.body.classList.remove("dark");
+  }
 }
 function es() {
   //console.log("Spanish");
